7430f60da02740557ffeab6f08f43bbe1900a188
Dayvson Silva <userddssilva@gmail.com>
2020-09-09 16:33:05 -0400

    Update Perceptron.py
    Criando um metodo para randomizar o conjunto de treino

--- src/Perceptron.py
+++ src/Perceptron.py
@@ -5,11 +5,12 @@
 import numpy as np
 
 class Perceptron(object):
-    def __init__(self, no_of_inputs, epoch=100, learning_rate=np.random.uniform(0.1, 0.4, 1), baias=-1):
+    def __init__(self, no_of_inputs, epoch=100, learning_rate=np.random.uniform(0.1, 0.4, 1), baias=-1, random_train_set=False):
         self.epoch = epoch
         self.learning_rate= learning_rate
         self.weights = np.random.uniform(-0.5, 0.5, no_of_inputs + 1)
         self.baias = baias
+        self.random_train_set = random_train_set
     
     def __add_baias(self, inputs):
         new_inputs = []
@@ -33,6 +34,15 @@ class Perceptron(object):
             return True
         else:
             return False
+
+    def __random_sets(self, x_train, y_train):
+        # Randomizando os dados de treino
+        index = np.arange(len(x_train))
+        np.random.shuffle(index)
+        x_train = x_train[index]
+        y_train = y_train[index]
+        return x_train, y_train
+        #---------------------------------
     
     def predict(self, x_test):
         x_test = self.__add_baias(x_test)
@@ -54,12 +64,8 @@ class Perceptron(object):
             print("Quantidade de Ajustes:", count_adjust)
             no_erro = True
 
-            # Randomizando os dados de treino
-            index = np.arange(len(x_train))
-            np.random.shuffle(index)
-            x_train = x_train[index]
-            y_train = y_train[index]
-            #---------------------------------
+            if self.random_train_set:
+                x_train, y_train = self.__random_sets(x_train, y_train)
 
             for x, y in zip(x_train, y_train):
                 u = np.sum(np.dot(x, self.weights))
